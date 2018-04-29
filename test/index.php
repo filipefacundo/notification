30f4a9a2f426595d4b59e969cd25959b65e744f6
Filipe Facundo <filipefbezerra@outlook.com>
2018-04-29 02:59:04 -0300

    TEST OK!

--- test/index.php
+++ test/index.php
@@ -4,6 +4,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 USE Notification\Email;
 
-$mail = new Email (2, "mail.host.com", "your@email.com", "your pass", "smtp secure (tls / ssl)", "language (example: 'br')", "from@email.com", "From the name");
+$mail = new Email(2, "mail.host.com", "your@email.com", "your pass", "smtp secure (tls / ssl)", "language (example: 'br')", "from@email.com", "From the name");
 
-$email-> sendEmail ("Subject", "Content", "reply@email.com", "Repetition Name", "address@email.com", "Address Name");
+$email-> sendEmail("Subject", "Content", "reply@email.com", "Repetition Name", "address@email.com", "Address Name");
